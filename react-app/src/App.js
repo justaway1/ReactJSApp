9dd9342bae555f25b0bcdb2f854385e14b6500da
justaway1 <44018646+justaway1@users.noreply.github.com>
2023-02-15 10:52:51 +0200

    Setting the env...

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './scss/App.scss'
+import Header from './Components/Header'
+import Content from './Components/Content'
+import Footer from './Components/Footer'
+
+function App () {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ react-app/src/Components/Content.js
@@ -0,0 +1,9 @@
+const Content = () => {
+  return (
+    <main>
+      <h1 className='custom_content'>Content</h1>
+    </main>
+  )
+}
+
+export default Content

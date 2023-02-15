d6e355601f27a79f88b7b9c78ecd78de2c2a15e8
justaway1 <44018646+justaway1@users.noreply.github.com>
2023-02-15 13:15:53 +0200

    Extracted content

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,14 @@ import './scss/App.scss'
 import Header from './Components/Header'
 import Content from './Components/Content'
 import Footer from './Components/Footer'
+import Employees from './Components/Employees'
 
 function App () {
   return (
     <div>
       <Header />
       <Content />
+      <Employees />
       <Footer />
     </div>
   )

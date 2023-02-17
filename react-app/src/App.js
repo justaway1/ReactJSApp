2b9f7ca83cab3319580b7a0f5b9dc88054711529
justaway1 <44018646+justaway1@users.noreply.github.com>
2023-02-17 11:42:48 +0200

    moved the state to the parent component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,125 @@
 import React from 'react'
+import { useState } from 'react'
 import './scss/App.scss'
 import Header from './Components/Header'
-import Content from './Components/Content'
 import Footer from './Components/Footer'
 import Employees from './Components/Employees'
 
 function App () {
+  const [selectedTeam, setSelectedTeam] = useState('TeamA')
+
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      fullName: 'Bob Jones',
+      designation: 'JavaScript Developer',
+      gender: 'male',
+      teamName: 'TeamA'
+    },
+    {
+      id: 2,
+      fullName: 'Jill Bailey',
+      designation: 'Node Developer',
+      gender: 'female',
+      teamName: 'TeamA'
+    },
+    {
+      id: 3,
+      fullName: 'Gail Shepherd',
+      designation: 'Java Developer',
+      gender: 'female',
+      teamName: 'TeamA'
+    },
+    {
+      id: 4,
+      fullName: 'Sam Reynolds',
+      designation: 'React Developer',
+      gender: 'male',
+      teamName: 'TeamB'
+    },
+    {
+      id: 5,
+      fullName: 'David Henry',
+      designation: 'DotNet Developer',
+      gender: 'male',
+      teamName: 'TeamB'
+    },
+    {
+      id: 6,
+      fullName: 'Sarah Blake',
+      designation: 'SQL Server DBA',
+      gender: 'female',
+      teamName: 'TeamB'
+    },
+    {
+      id: 7,
+      fullName: 'James Bennet',
+      designation: 'Angular Developer',
+      gender: 'male',
+      teamName: 'TeamC'
+    },
+    {
+      id: 8,
+      fullName: 'Jessica Faye',
+      designation: 'API Developer',
+      gender: 'female',
+      teamName: 'TeamC'
+    },
+    {
+      id: 9,
+      fullName: 'Lita Stone',
+      designation: 'C++ Developer',
+      gender: 'female',
+      teamName: 'TeamC'
+    },
+    {
+      id: 10,
+      fullName: 'Daniel Young',
+      designation: 'Python Developer',
+      gender: 'male',
+      teamName: 'TeamD'
+    },
+    {
+      id: 11,
+      fullName: 'Adrian Jacobs',
+      designation: 'Vue Developer',
+      gender: 'male',
+      teamName: 'TeamD'
+    },
+    {
+      id: 12,
+      fullName: 'Devin Monroe',
+      designation: 'Graphic Designer',
+      gender: 'male',
+      teamName: 'TeamD'
+    }
+  ])
+
+  function handleTeamSelectionChange (event) {
+    setSelectedTeam(event.target.value)
+  }
+
+  function handleEmployeeCardClick (event) {
+    const transformedEmployee = employees.map(employee =>
+      employee.id === parseInt(event.currentTarget.id)
+        ? employee.teamName === selectedTeam
+          ? { ...employee, teamName: '' }
+          : { ...employee, teamName: selectedTeam }
+        : employee
+    )
+
+    setEmployees(transformedEmployee)
+  }
+
   return (
     <div>
       <Header />
-      <Employees />
+      <Employees
+        employees={employees}
+        selectedTeam={selectedTeam}
+        handleEmployeeCardClick={handleEmployeeCardClick}
+        handleTeamSelectionChange={handleTeamSelectionChange}
+      />
       <Footer />
     </div>
   )
--- react-app/src/Components/Content.js
+++
@@ -1,9 +0,0 @@
-const Content = () => {
-  return (
-    <main>
-      <h1 className='custom_content'>Content</h1>
-    </main>
-  )
-}
-
-export default Content

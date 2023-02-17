323690b5ef48708530b346f4a4efb262700cee50
justaway1 <44018646+justaway1@users.noreply.github.com>
2023-02-17 11:10:50 +0200

    added styles to the card

--- react-app/src/Components/Employees.js
+++ react-app/src/Components/Employees.js
@@ -6,6 +6,8 @@ import maleProfile from '../images/maleProfile.jpg'
 const Employees = () => {
   // List of Employees
 
+  const [selectedTeam, setSelectedTeam] = useState('TeamB')
+
   const [employees, setEmployees] = useState([
     {
       id: 1,
@@ -93,18 +95,67 @@ const Employees = () => {
     }
   ])
 
+  function handleTeamSelectionChange (event) {
+    console.log(event.target.value)
+    setSelectedTeam(event.target.value)
+  }
+
+  function handleEmployeeCardClick (event) {
+    const transformedEmployee = employees.map(employee =>
+      employee.id === parseInt(event.currentTarget.id)
+        ? employee.teamName === selectedTeam
+          ? { ...employee, teamName: '' }
+          : { ...employee, teamName: selectedTeam }
+        : employee
+    )
+
+    setEmployees(transformedEmployee)
+  }
+
   return (
     <main className='container'>
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-6'>
+          <select
+            className='form-select form-select-lg'
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
+            <option value='TeamA'>TeamA</option>
+            <option value='TeamB'>TeamB</option>
+            <option value='TeamC'>TeamC</option>
+            <option value='TeamD'>TeamD</option>
+          </select>
+        </div>
+      </div>
+
       <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-8'>
           <div className='card-collection'>
             {employees.map(employee => (
-              <div className='card m-2' id={employee.id}>
-                <img
-                  src={femaleProfile}
-                  className='card-img-top'
-                  alt='femaleProfile'
-                />
+              <div
+                className={
+                  employee.teamName === selectedTeam
+                    ? 'card m-2 standout'
+                    : 'card m-2'
+                }
+                id={employee.id}
+                onClick={handleEmployeeCardClick}
+              >
+                {/* Check with ternary operator for gender  */}
+                {employee.gender === 'male' ? (
+                  <img
+                    src={maleProfile}
+                    className='card-img-top'
+                    alt='maleProfile'
+                  />
+                ) : (
+                  <img
+                    src={femaleProfile}
+                    className='card-img-top'
+                    alt='femaleProfile'
+                  />
+                )}
                 <div className='card-body'>
                   <h3 className='card-title'>FullName: {employee.fullName}</h3>
                   <p className='card-text'>
